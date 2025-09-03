cf9df5b2bbd6b1a4408bba77c3c48be051c4da03
franpsS2 <francianepereira4545@gmail.com>
2025-09-03 14:13:39 -0300

    03/09/2025

--- my-portfolio/src/components/Banner.tsx
+++ my-portfolio/src/components/Banner.tsx
@@ -34,10 +34,10 @@ const Banner = ({ isPlaying, setIsPlaying }:BannerProps) => {
 
       {/* Animated Background Elements */}
       <div className="absolute inset-0">
-        {[...Array(100)].map((_, i) => (
+        {[...Array(40)].map((_, i) => (
           <motion.div
             key={i}
-            className="absolute w-1 h-1 bg-white/60 rounded-full"
+            className="absolute w-1 h-1 bg-white/80 rounded-full"
             initial={{
               x: Math.random() * window.innerWidth,
               y: Math.random() * window.innerHeight,
